ac06bcde35f41192af1a628a638e6ea709a09940
W <W@DESKTOP-6HNR67F>
2022-04-25 15:11:25 +0900

    두번째

--- test/src/main/java/com/user/UserDTO.java
+++ test/src/main/java/com/user/UserDTO.java
@@ -2,6 +2,9 @@ package com.user;
 
 public class UserDTO {
  private String userId;
+ private String userName;
+ 
+
 
 public String getUserId() {
 	return userId;
@@ -10,4 +13,12 @@ public String getUserId() {
 public void setUserId(String userId) {
 	this.userId = userId;
 }
+public String getUserName() {
+	return userName;
+}
+
+public void setUserName(String userName) {
+	this.userName = userName;
+}
+
 }
